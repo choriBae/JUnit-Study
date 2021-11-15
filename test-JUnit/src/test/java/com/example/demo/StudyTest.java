15faba3ec293daddc2c06c33885e31761e6b1033
chori <chori.dev@gmail.com>
2021-11-15 19:12:33 +0900

    - ArgumentsAggregator 추가학습 - 테스트 인스턴스 학습

--- test-JUnit/src/test/java/com/example/demo/StudyTest.java
+++ test-JUnit/src/test/java/com/example/demo/StudyTest.java
@@ -4,8 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
+import static org.mockito.Mockito.lenient;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledIf;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
@@ -14,7 +16,12 @@ import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
@@ -27,8 +34,11 @@ import org.springframework.beans.factory.annotation.Value;
 
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) //해당 클래스 전역에 이름에 _를 공백으로 치완한다.
+@TestInstance(Lifecycle.PER_CLASS) //클래스 마다 인스턴스를 생성하기 때문에, 이 클래스에 있는 모든 메서드가 하나의 인스턴스를 공유하게 된다.
 class StudyTest {
 
+	int value = 1;
+	
 	@Test
 	@DisplayName("스터디 만들기 ╯°□°）╯") //test이름을 원하는 문자로 변경 할 수 있다. 이게 더 좋아보임
 	@EnabledOnOs(OS.WINDOWS)
@@ -40,17 +50,17 @@ class StudyTest {
 	@FastTest
 	void test() {
 		//fail("Not yet implemented");
-		Study actual = new Study(10);	//생성자 만들기
+		//Study actual = new Study(10);	//생성자 만들기
 
-		System.out.println(System.getProperty("java.version"));
-		System.out.println(System.getProperty("java.vendor"));
-		System.out.println(System.getProperty("java.home"));
+		//System.out.println(System.getProperty("java.version"));
+		//System.out.println(System.getProperty("java.vendor"));
+		//System.out.println(System.getProperty("java.home"));
 		
-		String test_env = System.getenv("TEST_ENV");
-		System.out.println(test_env);
-		assumeTrue("LOCAL".equalsIgnoreCase(test_env));	//로컬인 경우에만 테스트를 실행한다.(조건)
+		//String test_env = System.getenv("TEST_ENV");
+		//System.out.println(test_env);
+		//assumeTrue("LOCAL".equalsIgnoreCase(test_env));	//로컬인 경우에만 테스트를 실행한다.(조건)
 		 
-		assumingThat("LOCAL".equalsIgnoreCase(test_env), ()-> assertThat(actual.getLimit()).isGreaterThan(0));
+		//assumingThat("LOCAL".equalsIgnoreCase(test_env), ()-> assertThat(actual.getLimit()).isGreaterThan(0));
 		
 		/*
 		assertTimeout(Duration.ofMillis(300), () -> { //300밀리초를 설정하여 테스트를 진행
@@ -74,6 +84,27 @@ class StudyTest {
 		);
 		*/
 		System.out.println("test1");
+	}
+	
+	//테스트 간 의존성을 없게 만들어야 한다.
+	//의존성이 있을 경우 실행순서에 문제가 생긴다.
+	@Test
+	@FastTest
+	void test1_tt_pp() {
+		System.out.println(this);
+		System.out.println(value++);
+		Study actual = new Study(value++);
+		assertThat(actual.getLimit()).isGreaterThan(0);
+	}
+	
+	@Test
+	//@Tag("slow")
+	@SlowTest
+	//@Disabled //사용하지 않는 메서드일 경우에 disabled한다.
+	void test2_tt_pp() {
+		System.out.println(this);
+		Study actual = new Study(value++);
+		System.out.println(value++);
 	}
 	
 	@DisplayName("반목문 테스트1")
@@ -114,6 +145,29 @@ class StudyTest {
 		System.out.println(study.getLimit());
 	}
 	
+	@DisplayName("반목문 테스트5-1")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+	void parameterizedTest4(ArgumentsAccessor argumentsAccessor) { //여러개의 인자값들을 받아 조합해서 만든 인스턴스를 테스트 할 수 있다.
+		Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+		System.out.println(study);
+	}
+	
+	@DisplayName("반목문 테스트5-2")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+	void parameterizedTest5(@AggregateWith(StudyAggregator.class) Study study) { //StudyAggregator메서드를 사용하려면 @AggregateWith 어노테이션을 사용한다.
+		System.out.println(study);
+	}
+	
+	//인스턴스를 만들지 않고 커스텀 메서드를 만들어 사용하려면.
+	static class StudyAggregator implements ArgumentsAggregator { //제약조건 : 반드시 static inner class거나 Public class 여야 한다.
+		@Override
+		public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) {
+			return new Study(accessor.getInteger(0), accessor.getString(1)); //0번째 값의 int형 값과, 1번째 값의 String형의 값을 조합하여 리턴한다.
+		} 
+	}
+	
 	static class StudyConverter extends SimpleArgumentConverter {
 
 		@Override
@@ -124,19 +178,11 @@ class StudyTest {
 		
 	}
 
-	/*
-	@Test
-	//@Tag("slow")
-	@SlowTest
-	@Disabled //사용하지 않는 메서드일 경우에 disabled한다.
-	void test2_tt_pp() {
-		//fail("Not yet implemented");
-		System.out.println("test2");
-	}
-	*/
-	
+	//@TestInstance를 클래스당 만들면 static일 필요 없다.
+	//현재 클래스에서 인스턴스를 만들기 때문에, 메서드들이 static일 필요 없다.
 	@BeforeAll	//모든 test코드가 실행되기 전에 딱 한번 호출된다.
-	static void beforeAll() { //BeforeAll을 구현할 때는 static을 사용해야 한다, 리턴 타입이 존재할 수 없고 void만 가능하다.
+	//static void beforeAll() { //BeforeAll을 구현할 때는 static을 사용해야 한다, 리턴 타입이 존재할 수 없고 void만 가능하다.
+	void beforeAll() { 
 		System.out.println("before all");
 	}
 	
